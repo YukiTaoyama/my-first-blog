2256f05c2041765d375200630f79e7f6462ab9ba
Yuki Takahashi <a5816050@aoyama.jp>
2018-06-18 18:06:42 +0900

    My Django Girls app, third commit

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 steps = [6543, 7000, 8900, 10789, 3467, 11045, 5095]
 labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 num_bars = len(steps)
+colorlist = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33']
 positions = range(1, num_bars+1)
-plt.barh(positions, steps, align='center')
-plt.yticks(positions, labels)
-plt.xlabel('Steps')
-plt.ylabel('Day')
+plt.bar(positions, steps, color = ["r","g","b"] ,align='center')
+plt.xticks(positions, labels)
+plt.ylabel('Steps')
+plt.xlabel('Day')
 plt.title('Number of steps walked')
-plt.grid()
 plt.show()
